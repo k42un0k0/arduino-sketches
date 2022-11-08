b89aa56b3ea9ea6d82367d34ac3bc2e7ebe28a1a
k42un0k0 <k42un0k0.put1put1@gmail.com>
2022-11-08 18:16:18 +0900

    use timer in counter.ino

--- counter/counter.ino
+++ counter/counter.ino
@@ -1,36 +1,42 @@
-
-
+#include <MsTimer2.h>
+// pin
 int RESET = 3;
 int PLUS = 2;
 
+// global values
 int counter = 0;
-void reset();
-void plus();
 
-void setup()
-{
-    Serial.begin(9600);
-    Serial.println("Counter Start!");
-    pinMode(RESET, INPUT_PULLUP);
-    pinMode(PLUS, INPUT_PULLUP);
+// for controlling button
+int prevPlusValue = HIGH;
+int prevResetValue = HIGH;
+void analyzeButtonState();
 
-    attachInterrupt(0, reset, FALLING);
-    attachInterrupt(1, plus, FALLING);
+// arduino functions
+void setup() {
+  Serial.begin(9600);
+  Serial.println("Counter Start!");
+  pinMode(RESET, INPUT_PULLUP);
+  pinMode(PLUS, INPUT_PULLUP);
+  MsTimer2::set(50, analyzeButtonState);
+  MsTimer2::start();
 }
-void loop()
-{
-    Serial.println(counter);
-    delay(1000);
+void loop() {
+  Serial.println(counter);
+  delay(1000);
 }
 
-void reset()
-{
+// implements
+void analyzeButtonState() {
+  int resetValue = digitalRead(RESET);
+  if (resetValue == LOW && resetValue != prevResetValue) {
     Serial.println("Reset!");
     counter = 0;
-}
-
-void plus()
-{
-    Serial.println("PLUS!");
+  }
+  prevResetValue = resetValue;
+  int plusValue = digitalRead(PLUS);
+  if (plusValue == LOW && plusValue != prevPlusValue) {
+    Serial.println("Plus!");
     counter++;
-}
+  }
+  prevPlusValue = plusValue;
+};
